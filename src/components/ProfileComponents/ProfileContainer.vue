4345566f59c84b0fa4d019aa7247b0c5b83ffd0f
Alexis Plettener Nguyen <alexisplettener@gmail.com>
2022-12-21 18:54:09 +0000

    add prop to ProfileContainer and ProfileInfoContainer

--- src/components/ProfileComponents/ProfileContainer.vue
+++ src/components/ProfileComponents/ProfileContainer.vue
@@ -1,88 +1,42 @@
 <script lang="ts">
-    import { getUserInfo, loginUser } from '@/modules/getUserInfo';
-    export default {
-        name: 'ProfileContainer',
-        props: {
-            firstname: {
-                type: String,
-                required: true
-            },
-            lastname: {
-                type: String,
-                required: true
-            },
-            role: {
-                type: String,
-                required: true
-            },
-            about: {
-                type: String,
-                required: true
-            }
-        },
-        mounted() {
-            this.getLogin()
-            this.getUsers()
-        },
-        methods: {
-            async getLogin() {
-                const URL = "http://localhost:3000/auth/login"
-                console.log(await loginUser(URL, {
-                    username: "romer3",
-                    password: "Romer1234r"
-                }))
-            },
-            async getUsers(){ 
-                const user = await getUserInfo("http://localhost:3000/user/username/shincry", "GET")
-                const { firstname, lastname, role, about } = user
-                this.firstname = firstname
-                this.lastname = lastname
-                this.role = role
-                this.about = about
-            },
-            async getLogout(){
-                console.log(await getUserInfo("http://localhost:3000/auth/logout", "POST"))
-            }
-        },
-        data() {
-            return {
-                firstname: this.firstname,
-                lastname: this.lastname,
-                role: this.role,
-                about: this.about
-            }
-        }
-        
-    };
+export default {
+  name: 'ProfileContainer',
+  props: {
+    user: {
+      type: Object,
+      required: true
+    }
+  }
+}
 </script>
 <template>
-    <div class="profile-container">
-        <div class="image-container">
-            <img src="/img/ppic.jpeg" alt="" class="profile-img">
-        </div>
-        <div class="name-surname">
-            <p>{{ firstname }}{{ lastname }}</p>
-            <a><img src="/img/edit.png" alt="edit icon" class="edit-icon"></a>
-        </div>
-        <p>{{role}}</p>
-        <p>{{about}}</p>
+  <div class="profile-container">
+    <div class="image-container">
+      <img src="/img/ppic.jpeg" alt="" class="profile-img">
     </div>
+    <div class="name-surname">
+      <p>{{ user.firstname }}{{ user.lastname }}</p>
+      <a><img src="/img/edit.png" alt="edit icon" class="edit-icon"></a>
+    </div>
+    <p>{{ user.role }}</p>
+    <p>{{ user.about }}</p>
+  </div>
 </template>
 <style lang="scss">
-    .profile-container {
-        @apply w-3/12 h-[450px] flex flex-col items-center justify-center gap-2 bg-white border-2 border-slate-400 rounded-lg;
-        .image-container {
-            @apply rounded-full mb-6;
-            .profile-img {
-                @apply w-32 h-32 rounded-full border-2 border-secondary-light;
-            }
-        }
-
-        .name-surname {
-            @apply flex items-center justify-center gap-2 text-2xl font-bold text-secondary-dark;
-            .edit-icon {
-                @apply w-7;
-            }
-        }
+.profile-container {
+  @apply w-3/12 h-[450px] flex flex-col items-center justify-center gap-2 bg-white border-2 border-slate-400 rounded-lg;
+  .image-container {
+    @apply rounded-full mb-6;
+    .profile-img {
+      @apply w-32 h-32 rounded-full border-2 border-secondary-light;
     }
+  }
+
+  .name-surname {
+    @apply flex items-center justify-center gap-2 text-2xl font-bold text-secondary-dark;
+    .edit-icon {
+      @apply w-7;
+    }
+  }
+}
 </style>
--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,94 +1,129 @@
 <script lang="ts">
-    import ProfileContainer from "@/components/ProfileComponents/ProfileContainer.vue";
-    import ProfileInfoContainer from "@/components/ProfileComponents/ProfileInfoContainer.vue";
-    export default {
-        name: "ProfileView",
-        components: {
-            ProfileContainer,
-            ProfileInfoContainer
-        },
+import ProfileContainer from "@/components/ProfileComponents/ProfileContainer.vue";
+import ProfileInfoContainer from "@/components/ProfileComponents/ProfileInfoContainer.vue";
+import {getUserInfo, loginUser} from '@/modules/getUserInfo';
+
+export default {
+  name: "ProfileView",
+  components: {
+    ProfileContainer,
+    ProfileInfoContainer
+  },
+  data() {
+    return {
+      user: {
+        firstname: '',
+        lastname: '',
+        email: '',
+        role: '',
+        phone: '',
+        about: ''
+      }
     }
+  },
+  created() {
+    loginUser("http://localhost:3000/auth/login", {
+      username: "Alexis",
+      password: "Alexis123456",
+    }).then(() => {
+      getUserInfo("http://localhost:3000/user/username/Alexis", "GET").then((res) => {
+        console.log(res);
+        const newUser = {
+          firstname: res.firstname,
+          lastname: res.lastname,
+          email: res.email,
+          role: res.role,
+          phone: res.phone,
+          about: res.about
+        }
+        this.user = newUser;
+      });
+    });
+  }
+}
 </script>
 
 <template>
-    <div class="w-full flex flex-col gap-4 bg-secondary-light py-2 px-5">
-        <div class="w-full flex gap-4">
-            <ProfileContainer />
-            <ProfileInfoContainer />
-        </div>
-        <div class="social-media-container">
-            <div class="social-media">
-                <p>Social Media</p>
-            </div>
-            <div class="media-container">
-                <div class="icon-media-container">
-                    <img src="/img/trello.svg" alt="" class="icon">
-                    <p>Trello</p>
-                </div>
-                <div class="account">
-                    <p>John Smith</p>
-                </div>
-            </div>
-            <div class="media-container">
-                <div class="icon-media-container">
-                    <img src="/img/github.svg" alt="" class="icon">
-                    <p>Github</p>
-                </div>
-                <div class="account">
-                    <p>John Smith the best developer</p>
-                </div>
-            </div>
-            <div class="media-container">
-                <div class="icon-media-container">
-                    <img src="/img/linkedin.svg" alt="" class="icon">
-                    <p>LinkedIn</p>
-                </div>
-                <div class="account">
-                    <p>John Smith</p>
-                </div>
-            </div>
-            <div class="media-container">
-                <div class="icon-media-container">
-                    <img src="/img/facebook.svg" alt="" class="icon">
-                    <p>Facebook</p>
-                </div>
-                <div class="account">
-                    <p>Sr. Smith</p>
-                </div>
-            </div>
-            <div class="media-container">
-                <div class="icon-media-container">
-                    <img src="/img/whatsapp.svg" alt="" class="icon">
-                    <p>WhatsApp</p>
-                </div>
-                <div class="account">
-                    <p>+34666777420</p>
-                </div>
-            </div>
-        </div>
+  <div class="w-full flex flex-col gap-4 bg-secondary-light py-2 px-5">
+    <div class="w-full flex gap-4">
+      <ProfileContainer :user="user"/>
+      <ProfileInfoContainer :user="user"/>
     </div>
+    <div class="social-media-container">
+      <div class="social-media">
+        <p>Social Media</p>
+      </div>
+      <div class="media-container">
+        <div class="icon-media-container">
+          <img src="/img/trello.svg" alt="" class="icon">
+          <p>Trello</p>
+        </div>
+        <div class="account">
+          <p>John Smith</p>
+        </div>
+      </div>
+      <div class="media-container">
+        <div class="icon-media-container">
+          <img src="/img/github.svg" alt="" class="icon">
+          <p>Github</p>
+        </div>
+        <div class="account">
+          <p>John Smith the best developer</p>
+        </div>
+      </div>
+      <div class="media-container">
+        <div class="icon-media-container">
+          <img src="/img/linkedin.svg" alt="" class="icon">
+          <p>LinkedIn</p>
+        </div>
+        <div class="account">
+          <p>John Smith</p>
+        </div>
+      </div>
+      <div class="media-container">
+        <div class="icon-media-container">
+          <img src="/img/facebook.svg" alt="" class="icon">
+          <p>Facebook</p>
+        </div>
+        <div class="account">
+          <p>Sr. Smith</p>
+        </div>
+      </div>
+      <div class="media-container">
+        <div class="icon-media-container">
+          <img src="/img/whatsapp.svg" alt="" class="icon">
+          <p>WhatsApp</p>
+        </div>
+        <div class="account">
+          <p>+34666777420</p>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <style>
-    .social-media-container{
-        @apply bg-white rounded-lg;
-        .social-media {
-            @apply bg-secondary px-10 py-2 text-[18px] border-b-0 border-2 border-slate-400 rounded-t-lg font-spaceGrotesk text-3xl font-bold tracking-wide;
-        }
+.social-media-container {
+  @apply bg-white rounded-lg;
 
-        .media-container {
-            @apply px-12 grid grid-cols-2 text-left border-b-0 border-2 border-slate-400 py-3;
-            .icon-media-container {
-                @apply flex justify-start gap-12;
-            }
+  .social-media {
+    @apply bg-secondary px-10 py-2 text-[18px] border-b-0 border-2 border-slate-400 rounded-t-lg font-spaceGrotesk text-3xl font-bold tracking-wide;
+  }
 
-            .icon {
-                @apply w-7 h-7;
-            }
-        }
+  .media-container {
+    @apply px-12 grid grid-cols-2 text-left border-b-0 border-2 border-slate-400 py-3;
 
-        .media-container:last-child {
-            @apply border-b-2 rounded-b-lg;
-        }
+    .icon-media-container {
+      @apply flex justify-start gap-12;
     }
+
+    .icon {
+      @apply w-7 h-7;
+    }
+  }
+
+  .media-container:last-child {
+    @apply border-b-2 rounded-b-lg;
+  }
+}
 </style>
